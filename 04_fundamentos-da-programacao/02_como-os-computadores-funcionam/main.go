025d69579ea3c5f0ce36cd4c7132c7b4da897f3b
fabiokusaba <fabinhokb44@gmail.com>
2024-09-08 11:07:14 -0300

    aula 02 - como os computadores funcionam

---
+++ 04_fundamentos-da-programacao/02_como-os-computadores-funcionam/main.go
@@ -0,0 +1,38 @@
+package main
+
+func main() {
+
+}
+
+// Computadores funcionam com eletricidade.
+// A eletricidade pode estar ligada ou desligada.
+// Com esses esquemas de ligado e desligado podemos criar codificações atribuindo significado aos estados on e off.
+// Lâmpadas na varanda -> por exemplo se eu tenho uma lâmpada ela pode estar acesa ou apagada, e com uma lâmpada eu posso ter
+// duas mensagens: posso ter um significado para a lâmpada acesa e um significado para a lâmpada apagada. A fórmula é sempre
+// 2^n.
+// Esquemas de codificação -> a gente atribui significado a valores binários.
+// ON & OFF, 1 & 0, dígitos binários, bits e linguagem de máquina são termos utilizados para se referir a ideia que, em um
+// computador, é tudo um monte de zero e um. Os "interruptores" estão sempre ligados ou desligados, sendo dígitos binários, bits,
+// e esta é a linguagem que os computadores falam, esta é a linguagem de máquina.
+// ASCII -> esquema de codificação .
+// UTF-8 -> outro sistema de codificação que é um superset que inclui o ASCII.
+
+// Quantificando bits:
+// 1 bit
+// 8 bits = byte
+// 1000 bytes = kb
+// 1000 kb = mb
+// 1000 mb = gb
+// 1000 gb = tb
+
+// ENIAC -> foi criado para fazer cálculos balísticos. Ele tinha 16.000 "interruptores", ou seja, 16.000 0 & 1, esses "interruptores"
+// utilizavam válvulas termiônicas (vacuum tubes).
+// Depois da utilização das válvulas termiônicas foi criado nos anos 50 o transistor feito de semicondutores que tem propriedades
+// elétricas especiais.
+// Depois dos transistors vieram os chips que eram basicamente um monte de transistors dentro de um negocinho bem pequenininho.
+// Essa conta de, por exemplo tinha x válvulas termiônicas no sistema, aí quando apareceu transistor tinha mais transistors, aí quando
+// apareceu circuito integrado tinha mais transistor ainda embutido no circuito integrado, etc. Essa subida na quantidade de "interruptores"
+// dentro de um sistema computacional se chama Lei de Moore e o que ele dizia é que a cada ano o número de "interruptores" dentro de um
+// sistema computacional dobra.
+// Basicamente a gente tem cinco gerações de computadores: 1. ENIAC, 2. Transistores, 3. Circuitos integrados, 4. Microprocessadores,
+// 5. AI, machine learning.
