53a154d8823976b1e619734f7337e438ea74a75f
Fabio Kusaba <fabinhokb44@gmail.com>
2024-09-14 13:58:21 -0300

    aula 01 - entendendo fluxo de controle

---
+++ 06_fluxo-de-controle/01_entendendo-fluxo-de-controle/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+// Fluxo de controle é a maneira com que o programa de computador lê esse programa e executa ele.
+// Os programas de computador tem fluxos de controles distintos que eles podem usar para processar a informação.
+// Computadores lêem de cima para baixo, ou seja, todo software que você escrever ele vai ler de cima para baixo.
+// Ou seja, ele tem um fluxo de controle de leitura sequencial, ele lê em sequência uma linha depois da outra.
+// Além do fluxo de controle sequencial há duas declarações que podem afetar como o computador lê esse código para que
+// ele leia de uma maneira não sequencial. Uma é o fluxo de controle de repetição, o computador vai repetir a leitura
+// de um mesmo código de uma maneira específica, e o fluxo de controle de repetição também é chamado de fluxo de controle
+// iterativo.
+// E além disso a gente tem outro fluxo de controle que é o fluxo de controle condicional ou fluxo de controle de seleção.
+// Nesse caso o computador encontra uma condição e através de uma declaração if ou switch toma um curso ou outro dependendo
+// dessa condição.
+// Ou seja, há três tipos de fluxo de controle: sequencial, de repetição e condicional.
