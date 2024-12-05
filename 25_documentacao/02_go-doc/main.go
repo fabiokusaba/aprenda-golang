18e704c5cd43770cebd7d02fd4b616b5b6564521
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-05 20:52:45 -0300

    aula 02 - go doc

---
+++ 25_documentacao/02_go-doc/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+}
+
+// Comando da linha de comando.
+// Case sensitive, ou seja, ele pode diferenciar maiúsculas e minúsculas ou não.
+// Pode mostrar a documentação de campos não exportados tanto quanto de exportados.
