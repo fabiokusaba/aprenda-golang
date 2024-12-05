a2574045d601576393259126a35ff05fb53e85ae
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-05 20:46:59 -0300

    aula 01 - introdução

---
+++ 25_documentacao/01_introducao/main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+}
+
+// golang.org -> documentação da standard library
+// godoc.org -> documentação da standard library e outros
+// go doc -> comando para ler documentação na linha de comando
+// godoc -> idem acima, documentação que da para servir os arquivos como http local, ou seja, posso ter uma página web
+// com os packages que tem instalados no meu computador e vou poder ler no browser
