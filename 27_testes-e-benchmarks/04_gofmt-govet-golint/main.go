00b63568132c6df0b5d86fba0d4758f818a99873
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-06 20:16:18 -0300

    gofmt, go vet e golint

---
+++ 27_testes-e-benchmarks/04_gofmt-govet-golint/main.go
@@ -0,0 +1,10 @@
+package main
+
+func main() {
+
+}
+
+// gofmt: formata o código
+// go vet: correctness -> ela quer que as coisas estejam corretas, que estejam feitas da maneira certa, então se tiver
+// erros ele vai te dizer que tem erros, se não tiver erros ele não vai falar nada.
+// golint: suggestions -> sugestões, então ele procura por estilos de códigos que estão mais ou menos.
