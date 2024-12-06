8f93d9c92865fd90609636716942b0116900dbf0
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-05 21:12:52 -0300

    aula 04 - godoc.org

---
+++ 25_documentacao/04_godoc-org/main.go
@@ -0,0 +1,9 @@
+package main
+
+func main() {
+
+}
+
+// godoc.org
+// Quando criamos packages nós podemos botar os nossos packages no godoc.org para que outras pessoas possam acessar a
+// documentação dela.
