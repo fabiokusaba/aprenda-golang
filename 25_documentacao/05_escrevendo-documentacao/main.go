c284126690889aff3c199edb7752fbb9417ff210
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-05 21:33:00 -0300

    aula 05 - escrevendo documentação

---
+++ 25_documentacao/05_escrevendo-documentacao/main.go
@@ -0,0 +1,16 @@
+package main
+
+func main() {
+
+}
+
+// Documentação é uma parte extremamente importante de fazer com que o software seja acessível e sustentável.
+// Documentação deve ser bem escrita e correta, mas também fácil de escrever e manter.
+// A documentação deve ser acoplada com o código e evoluir junto com este. Quanto mais fácil for para os programadores
+// criarem boa documentação melhor fica pra todos os envolvidos.
+
+// Um comentário de uma linha.
+
+/*
+	Pode ser um comentário de mais de uma linha.
+*/
