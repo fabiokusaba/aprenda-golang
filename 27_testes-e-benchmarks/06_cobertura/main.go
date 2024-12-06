69feec0bc0969dfbf028073764f04fb757c3cf6e
Fabio Kusaba <fabinhokb44@gmail.com>
2024-12-06 20:42:34 -0300

    update file

--- 27_testes-e-benchmarks/06_cobertura/main.go
+++ 27_testes-e-benchmarks/06_cobertura/main.go
@@ -3,3 +3,9 @@ package main
 func main() {
 
 }
+
+// Cobertura quando estamos falando de testes se trata de qual porção do seu package está sendo coberto por testes, ou
+// seja, quantos % do seu código está sendo testado e quantos % do seu código está sendo "arriscado".
+// Via de regra, uma taxa de cobertura de 70-80% está bom.
+// A flag -cover faz a análise de cobertura do nosso código.
+// Podemos utilizar a flag -coverprofile <arquivo> para salvar a análise de cobertura em um arquivo.
